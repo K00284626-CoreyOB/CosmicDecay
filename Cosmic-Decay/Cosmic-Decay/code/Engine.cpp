0ff0b1a9d5e2e96406eb1afb3a639d0f58a81c12
K00284626-CoreyOB <K00284626@student.tus.ie>
2024-10-08 15:46:50 +0100

    TextureHolder, Engine

---
+++ Cosmic-Decay/Cosmic-Decay/code/Engine.cpp
@@ -0,0 +1,15 @@
+#include "Engine.h"
+
+/* Author: Corey O'Brien  Version 1.0
+Program Description: This class will be the Engine which will store most of the data needed.
+Calls the run() method to initialise the game.*/
+
+Engine::Engine()
+{
+
+}
+
+void Engine::run()
+{
+
+}
--- Cosmic-Decay/Cosmic-Decay/code/Engine.h
+++ Cosmic-Decay/Cosmic-Decay/code/Engine.h
@@ -1,24 +1,49 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include "Player.h"
+#include "Arena.h"
+#include "TextureHolder.h"
+#include "loadBackground.h"
 
 //using namespace sf;
 
 class Engine
 {
 private:
-	/*Player p;
-
-	Enemy e;
-
-	Gun g;
-
+	//The texture holder
 	TextureHolder Th;
 
-	LevelLoader l;
+	// The game will always be in one of four states
+	enum class State { PAUSED, LEVELING_UP, GAME_OVER, PLAYING };
 
-	Arena a;*/
+	// Start with the GAME_OVER state
+	State state = State::GAME_OVER;
+
+	// Get the screen resolution and create an SFML window
+	Vector2f resolution;
+
+	// Create a an SFML View for the main action
+	View mainView;
+
+	// Here is our clock for timing everything
+	Clock clock;
+
+	// Where is the mouse in relation to world coordinates
+	Vector2f mouseWorldPosition;
+	// Where is the mouse in relation to screen coordinates
+	Vector2i mouseScreenPosition;
+
+	// Create an instance of the Player class
+	Player player;
+
+	// The boundaries of the arena
+	IntRect arena;
+
+	// Create the background
+	VertexArray background;
 
 public:
-	//Engine();
+	Engine();
 
-	//void run();
+	void run();
 };
