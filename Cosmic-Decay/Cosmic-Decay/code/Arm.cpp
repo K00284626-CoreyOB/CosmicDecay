64261072d7473e08d907eb51558ea916f0e6d9e9
Zach Ahearn <zachtahearn@gmail.com>
2024-10-31 14:37:39 +0000

    engine

--- Cosmic-Decay/Cosmic-Decay/code/Arm.cpp
+++ Cosmic-Decay/Cosmic-Decay/code/Arm.cpp
@@ -20,23 +20,3 @@ FloatRect Arm::getPosition()
 	return m_Sprite.getGlobalBounds();
 }
 
-
-/*
-void Arm::update(float elapsedTime, Vector2i mousePosition)
-{
-	// Set the Arm's position relative to the player's position
-	FloatRect playerPos = m_Player.getPosition();
-	m_Position.x = playerPos.left;  // Adjust this if necessary for your positioning
-	m_Position.y = playerPos.top;
-
-	// Update the sprite position
-	m_Sprite.setPosition(m_Position);
-
-	
-	float angle = (atan2(mousePosition.y - m_Resolution.y / 2,
-		mousePosition.x - m_Resolution.x / 2) * 180) / 3.141;
-	m_Sprite.setRotation(angle);
-
-	m_Sprite.setPosition(m_Position);
-}
-*/
